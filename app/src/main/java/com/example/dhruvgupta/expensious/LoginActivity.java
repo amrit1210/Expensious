7889ac84f2890b32a1110ae030e67e79c65fd87d
Dhruv Gupta <dhruvgupta174@gmail.com>
2015-02-27 19:27:45 +0530

    Accounts Layouts

--- app/src/main/java/com/example/dhruvgupta/expensious/LoginActivity.java
+++ app/src/main/java/com/example/dhruvgupta/expensious/LoginActivity.java
@@ -11,7 +11,7 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot);
+        setContentView(R.layout.activity_add_account);
     }
 
 
